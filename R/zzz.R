09df489d6743f8e2a648a0d8e267123e937cc72a
Rick Helmus <r.helmus@uva.nl>
2024-06-22 21:15:38 +0200

    link to zlibbioc

--- R/zzz.R
+++ R/zzz.R
@@ -4,15 +4,20 @@ pkgconfig <- function(opt = c("PKG_CFLAGS", "PKG_CXXFLAGS", "PKG_LIBS"))
     opt <- match.arg(opt)
 
     if (opt %in% c("PKG_CFLAGS", "PKG_CXXFLAGS"))
-        cat(sprintf("-I%s", system.file("include", package = "Rlibmstoolkit")))
+    {
+        zflags <- utils::capture.output(zlibbioc::pkgconfig("PKG_CFLAGS"))
+        cat(sprintf("-I%s", system.file("include", package = "Rlibmstoolkit")), zflags)
+    }
     else
     {
         arch <- if (nzchar(.Platform$r_arch))
             sprintf("/%s", .Platform$r_arch)
         else
             ""
-
+        zflags <- utils::capture.output(
+            zlibbioc::pkgconfig(paste0("PKG_LIBS_", if (Sys.info()["sysname"] == "Windows") "shared" else "static"))
+        )
         cat(sprintf("%s%s/lib%s.a", system.file("libs", package = "Rlibmstoolkit"), arch,
-            c("mstlite", "mzimltools", "mzparser", "expat")), "-lz")
+            c("mstlite", "mzimltools", "mzparser", "expat")), zflags)
     }
 }
