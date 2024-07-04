948d1e6130bf4ecca3463befcba4bf56352f256e
Rick Helmus <r.helmus@uva.nl>
2024-07-04 20:15:44 +0200

    few more compilation fixes

--- R/zzz.R
+++ R/zzz.R
@@ -6,7 +6,8 @@ pkgconfig <- function(opt = c("PKG_CFLAGS", "PKG_CXXFLAGS", "PKG_LIBS"))
     if (opt %in% c("PKG_CFLAGS", "PKG_CXXFLAGS"))
     {
         zflags <- utils::capture.output(zlibbioc::pkgconfig("PKG_CFLAGS"))
-        cat(sprintf("-I%s", system.file("include", package = "Rlibmstoolkit")), zflags)
+        cat(sprintf("-I%s", system.file("include", package = "Rmstoolkitlib")), "-D_LARGEFILE_SOURCE",
+            "-D_FILE_OFFSET_BITS=64", "-DGCC -DHAVE_EXPAT_CONFIG_H", zflags)
     }
     else
     {
@@ -14,10 +15,15 @@ pkgconfig <- function(opt = c("PKG_CFLAGS", "PKG_CXXFLAGS", "PKG_LIBS"))
             sprintf("/%s", .Platform$r_arch)
         else
             ""
-        zflags <- utils::capture.output(
-            zlibbioc::pkgconfig(paste0("PKG_LIBS_", if (Sys.info()["sysname"] == "Windows") "shared" else "static"))
-        )
-        cat(sprintf("%s%s/lib%s.a", system.file("libs", package = "Rlibmstoolkit"), arch,
+        zflags <- if (Sys.info()["sysname"] == "Windows")
+            utils::capture.output(zlibbioc::pkgconfig(paste0("PKG_LIBS_shared")))
+        else
+        {
+            # BUG? zlibbioc vignette recommends to link to static on Linux, but doesn't actually provide it. However, it
+            # is usually present anyway, so we can use the system zlib
+            "-lz"
+        }
+        cat(sprintf("%s%s/lib%s.a", system.file("libs", package = "Rmstoolkitlib"), arch,
             c("mstlite", "mzimltools", "mzparser", "expat")), zflags)
     }
 }
