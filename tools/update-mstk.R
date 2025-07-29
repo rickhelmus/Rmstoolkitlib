035d77b4bf017386ff98d4a3aa1c8810c366da69
Rick Helmus <r.helmus@uva.nl>
2025-07-29 09:17:08 +0200

    temp switch to fork

--- tools/update-mstk.R
+++ tools/update-mstk.R
@@ -1,4 +1,4 @@
-ghurl <- "https://github.com/mhoopmann/mstoolkit/archive/refs/heads/master.zip"
+ghurl <- "https://github.com/rickhelmus/mstoolkit/archive/refs/heads/timsfix.zip"
 outf <- tempfile(fileext = ".zip")
 stopifnot(download.file(ghurl, outf, mode = "wb") == 0)
 
@@ -7,7 +7,7 @@ srcdir <- "src"
 exdir <- file.path(srcdir, "mstoolkit")
 unlink(exdir, recursive = TRUE)
 unzip(outf, exdir = srcdir)
-file.rename(file.path(srcdir, "mstoolkit-master"), file.path(srcdir, "mstoolkit"))
+file.rename(file.path(srcdir, "mstoolkit-timsfix"), file.path(srcdir, "mstoolkit"))
 
 # generated with: diff -u --label original src/mstoolkit/Makefile --label modified tools/Makefile > tools/Makefile.patch
 stopifnot(system2("patch", c(file.path(exdir, "Makefile"), file.path("tools", "Makefile.patch"))) == 0)
